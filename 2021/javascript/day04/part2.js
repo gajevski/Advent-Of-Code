3e8cd27c7871c66c5882aa241f195d22e172709a
gajevski <mikolajxgajewski@gmail.com>
2022-12-08 22:16:36 +0100

    Day04 part01

---
+++ 2021/javascript/day04/part2.js
@@ -0,0 +1,20 @@
+function countPairs(input) {
+    let answer = 0;
+    const data = input.split('\n');
+    const pairs = data.map(item => item.split(',').map(element => element.split('-').map(num =>
+      parseInt(num)
+    )));
+    pairs.forEach(item => {
+      const [a, b] = item[0];
+      const [x, y] = item[1];
+      if (( (a <= x) && (b >=y) ) || ( (a >= x) && (b <= y) )){
+        answer += 1;
+      }
+    });
+    return answer;
+  }
+  
+  const input = document.getElementById("app").innerHTML.trim();
+  
+  console.log(countPairs(input));
+  
