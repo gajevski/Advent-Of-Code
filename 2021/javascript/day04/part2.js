d55263a3fb862d355f0f14404fdee4ba5d3af435
gajevski <mikolajxgajewski@gmail.com>
2022-12-09 12:57:35 +0100

    Day04 part02

--- 2021/javascript/day04/part2.js
+++ 2021/javascript/day04/part2.js
@@ -2,19 +2,20 @@ function countPairs(input) {
     let answer = 0;
     const data = input.split('\n');
     const pairs = data.map(item => item.split(',').map(element => element.split('-').map(num =>
-      parseInt(num)
+        parseInt(num)
     )));
     pairs.forEach(item => {
-      const [a, b] = item[0];
-      const [x, y] = item[1];
-      if (( (a <= x) && (b >=y) ) || ( (a >= x) && (b <= y) )){
-        answer += 1;
-      }
+        const [a, b] = item[0];
+        const [x, y] = item[1];
+        if ((a < x && b < x) || (a > y && b > y)) {
+          answer += 0;
+        } else {
+          answer += 1;
+        }
     });
     return answer;
-  }
-  
-  const input = document.getElementById("app").innerHTML.trim();
-  
-  console.log(countPairs(input));
-  
+}
+
+const input = document.getElementById("app").innerHTML.trim();
+
+console.log(countPairs(input));
